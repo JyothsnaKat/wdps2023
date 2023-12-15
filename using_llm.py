8f73dedb7fe71aeb998f1a7b9c33941cc24a2980
tan9zj <tangzj021@gmail.com>
2023-12-15 16:45:44 +0100

    format input/output and simple integration

--- using_llm.py
+++ using_llm.py
@@ -1,33 +1,73 @@
 from ctransformers import AutoModelForCausalLM
+import spacy
 # data/word_vectors/glove.6B.100d.zip
-repository="TheBloke/Llama-2-7B-GGUF"
-model_file="llama-2-7b.Q4_K_M.gguf"
-llm = AutoModelForCausalLM.from_pretrained(repository, model_file=model_file, model_type="llama")
+# Step 1: Get the answer from llama
+def query_language_model(textA):
+    repository="TheBloke/Llama-2-7B-GGUF"
+    model_file="llama-2-7b.Q4_K_M.gguf"
+    llm = AutoModelForCausalLM.from_pretrained(repository, model_file=model_file, model_type="llama")
 
-# question list
-# the running time may be different, some of them are really costy and sometimes answers dont make sense
-questions = [
-    "The capital of the Netherlands is ...",
-    "Is Amsterdam the capital of the Netherlands?"
-    # "Purple is a mixture color of red and ... ",
-    # "Is purple a mixture color of red and blue?",
-    # "The nationality of Lana Del Rey is ...",
-    # "Is Lana Del Rey a French?",
-    # "The theory of relativity is proposed by ...",
-    # "Is Newton born in America?",
-    # "Van Gogh was born in ...",
-    # "Did Van Gogh commit suicide?"
-]
-answers = []
+    textB = llm(textA)
+    # print(f"Text returned by the language model (B)(llama 2, 70B): {textB}\n")
+    return textB
 
-print("Computing the answer (can take some time)...")
-# prompt = input("Type your question (for instance: \"The capital of Italy is \") and type ENTER to finish:\n")
-# print("Computing the answer (can take some time)...")
-for question in questions:
-    completion = llm(question)
-    answers.append(completion)
-    # print("compished\n")
     
-for i, answer in enumerate(answers):
-    print(f"Quetion {i+1}: {questions[i]}")
-    print(f"Answer: {answer}\n")
+# Step 2: Extract answer, yes or no or entity
+
+# Step 3: Fact checking
+# def fact_checking(textB):
+#     return checked_fact, confidence
+
+# Step 4: Using spacy to recognize entities
+def extract_entities(textA, textB):
+    # Load the spaCy English model
+    nlp = spacy.load("en_core_web_sm")
+    # Combine the two texts
+    combined_text = textA + " " + textB
+    # Process the combined text
+    doc = nlp(combined_text)
+    # Extract entities and their Wikipedia URLs
+    entities = set()  # Using set to avoid duplicates
+    for ent in doc.ents:
+        if ent.label_ in ["GPE", "LOC", "ORG"]:  # Consider entities like geopolitical entities, locations, and organizations
+            normalized_text = ent.text.capitalize()   # Normalize the text to first letter uppercase and others lowercase
+            entities.add((normalized_text, f"https://en.wikipedia.org/wiki/{normalized_text.replace(' ', '_')}"))
+    # Converting the set back to a list to return it
+    # print("Extracting")
+    return list(entities)
+
+
+# Process a txt as input and format the output
+
+def process_input(input_line):
+    print("Processing input")
+    question_id, question_textA = input_line.split("\t")
+    question_textB = query_language_model(question_textA)
+    entities = extract_entities(question_textA, question_textB)
+    # correctness = check_correctness(extracted_answer)
+    # return question_id, question_textB, extracted_answer, correctness, entities
+    # print("Processing input finished")
+    return question_id, question_textB, entities
+
+# def format_output(question_id, response, extracted_answer, correctness, entities):
+def format_output(question_id, question_textB, entities):
+    output = []
+    output.append(f"{question_id}\tR\"{question_textB}\"")
+    # output.append(f"{question_id}\tA\"{extracted_answer}\"")
+    # output.append(f"{question_id}\tC\"{correctness}\"")
+    for entity, url in entities:
+        output.append(f"{question_id}\tE\"{entity}\"\t\"{url}\"")
+    print("Formatting")
+    return "\n".join(output)
+
+# Main execution
+input_file = "/app/project/question_lists.txt"
+with open(input_file, "r") as file:
+    for line in file:
+        print("Reading file")
+        # question_id, question_textB, extracted_answer, correctness, entities = process_input(line.strip())
+        # formatted_output = format_output(question_id, question_textB, extracted_answer, correctness, entities)
+        question_id, question_textB, entities = process_input(line.strip())
+        formatted_output = format_output(question_id, question_textB, entities)
+    print(formatted_output)
+
