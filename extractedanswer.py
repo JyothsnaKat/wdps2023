6f1eb483631bc580f71b80322a870441ad96700c
JyothsnaKat <35322457+JyothsnaKat@users.noreply.github.com>
2023-12-17 17:44:56 +0100

    Update extractedanswer.py

--- extractedanswer.py
+++ extractedanswer.py
@@ -2,16 +2,12 @@ import spacy
 
 def extract_wikipedia_title_and_pattern(text):
     # Check if the word "Wikipedia" is in the text
-    if "Wikipedia" in text:
+    text = text.lower()
+    if "wikipedia" in text:
         text = "Wikipedia entity"
         return text, None
     else:
         return None, None
-
-def is_wikipedia_entity(entity):
-    # Placeholder function; you can implement a more sophisticated check here
-    # For simplicity, it checks if the entity is in lowercase (as Wikipedia URLs often are)
-    return entity and entity.islower()
 
 def extract_yes_no_answer(text):
     # Load the spaCy English model
@@ -21,8 +17,8 @@ def extract_yes_no_answer(text):
     doc = nlp(text)
 
     # Define a list of positive and negative keywords
-    positive_keywords = ["yes", "true", "correct", "affirmative"]
-    negative_keywords = ["no", "false", "incorrect", "negative", "not"]
+    positive_keywords = ["yes", "true", "correct", "affirmative", "not false"]
+    negative_keywords = ["no", "false", "incorrect", "negative", "not", "not true"]
 
     # Check for the presence of positive or negative keywords in the processed text
     for token in doc:
@@ -35,7 +31,7 @@ def extract_yes_no_answer(text):
     return None
 
 # Example text
-text = "Most people think Managua is the capital of Nicaragua. However, Managua is not the capital of Nicaragua.The capital of Nicaragua is Managua. The capital of Nicaragua is Managua. Managua is the capital of Nicaragua. The capita"
+text = "Wikipedia Most people think Managua is the capital of Nicaragua. However, Managua is not the capital of Nicaragua.The capital of Nicaragua is Managua. The capital of Nicaragua is Managua. Managua is the capital of Nicaragua. The capita"
 
 wikipedia_title, search_pattern = extract_wikipedia_title_and_pattern(text)
 
@@ -52,5 +48,3 @@ else:
         print(f"Extracted answer: {extracted_answer}")
     else:
         print("No yes/no answer was found.")    
-
-
